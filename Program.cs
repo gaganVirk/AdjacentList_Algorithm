8267268e8a63cfc212435355ca853297586ee216
gaganVirk <deep.virk@outlook.co.nz>
2019-07-17 13:48:26 +1200

    changed vertex class

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,33 @@ namespace AdjacentList_Algo
     {
         static void Main(string[] args)
         {
+            StreamReader reader = new StreamReader("AdjList.txt");
+            Console.SetIn(reader);
+
+            int size = Convert.ToInt32(Console.ReadLine());
+            List<Vertex> vertices = new List<Vertex>();
+
+            for(int v = 0; v < size; v++)
+            {
+                vertices.Add(new Vertex(v));
+            }
+
+            string line = Console.ReadLine();
+            while(line != "-1 -1")
+            {
+                string[] array = line.Split(' ');
+                int token1 = int.Parse(array[0]);
+                int token2 = int.Parse(array[1]);
+                vertices[token1].AddNeighbour(vertices[token2]);
+                line = Console.ReadLine();
+            }
+
+            foreach(Vertex v in vertices)
+            {
+                Console.WriteLine("{0}", v.ToString());
+            }
+          
+            Console.ReadLine();
         }
     }
 }
--- Vertex.cs
+++ Vertex.cs
@@ -8,5 +8,27 @@ namespace AdjacentList_Algo
 {
     class Vertex
     {
+        public int Id { get; set; }
+        List<Vertex> adj = new List<Vertex>();
+
+        public Vertex(int id)
+        {
+            this.Id = id;
+        }
+
+        public void AddNeighbour(Vertex v)
+        {
+            adj.Add(v);
+        }
+
+        public override string ToString()
+        {
+            string s = string.Format("{0}: ", Id);
+            foreach(Vertex v in adj)
+            {
+                s += string.Format(" {0}", v.Id);
+            }
+            return s;
+        }
     }
 }
